97dc157bdfb4c0f3d4873cbff08d9fb8b1949444
Miguel Ángel García <magarcial.dev@outlook.es>
2026-03-26 18:09:42 +0100

    Add new line to index

--- index.php
+++ index.php
@@ -5,4 +5,5 @@ echo "<p>Creador de la página: mgarlop</p>";
 echo "<p>línea añadida desde github</p>";
 echo "<button style=\"border-radius: 5px; border: 1px solid black; padding: 10px;\">Administración</button>";
 echo "<button style=\"border-radius: 5px; border: 1px solid black; padding: 10px;\">Lanzar Exploit :)</button>";
+echo "<p>Todos los derechos reservados</p>";
 ?>
