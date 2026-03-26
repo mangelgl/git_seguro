9fe9cf2dbaba8765f1fffae3b6023800b70ab2f1
danielgmciber-coder <danielgmciber@gmail.com>
2026-03-26 17:18:07 +0100

    nuevo boton añadido

--- index.php
+++ index.php
@@ -4,3 +4,5 @@ echo "<h1>Hola Mundo</h1>";
 echo "<p>Creador de la página: mgarlop</p>";
 echo "<p>línea añadida desde github</p>";
 echo "<button style=\"border-radius: 5px; border: 1px solid black; padding: 10px;\">Administración</button>";
+echo "<button style=\"border-radius: 5px; border: 1px solid black; padding: 10px;\">Lanzar Exploit :)</button>";
+?>
